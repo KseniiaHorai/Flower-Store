39c51c177b001d4b449eefca2265ec449d07f85b
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2025-11-30 21:25:59 +0200

    set up auth route and basic Express server

--- api/app.js
+++ api/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const authRoutes = require("./endpoints/auth.route.js");
+
+dotenv.config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Listening on port 4000");
