0857a0a219799208726cea0c945a9c4e1a7e7c07
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2026-01-18 21:47:39 +0200

    create User schema

---
+++ api/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "name cannot be empty"],
+        },
+        email: {
+            type: String,
+            required: [true, "email cannot be empty"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user",
+        },
+        password: {
+            type: String,
+            required: [true, "password cannot be empty"],
+            minlength: [8, "please use a password with 8+ characters"],
+        },
+        cartContents: [
+            {
+                quantity: {
+                    type: Number,
+                    default: 1,
+                },
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
