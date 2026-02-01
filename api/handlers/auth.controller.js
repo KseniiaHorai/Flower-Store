b81069066c11d32df080128fa9c3ca0b310f2a82
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2026-02-01 12:09:09 +0200

    add signup flow with JWT auth and Redis

--- api/handlers/auth.controller.js
+++ api/handlers/auth.controller.js
@@ -1,4 +1,45 @@
 const User = require("../models/User.js");
+const jwt = require("jsonwebtoken");
+const redis = require("../lib/redis.js");
+
+const getMyTokens = function (userId) {
+    const accessToken = jwt.sign({ userId }, process.env.SECRET_ACCESS_TOKEN, {
+        expiresIn: "10m",
+    });
+    const refreshToken = jwt.sign(
+        { userId },
+        process.env.SECRET_REFRESH_TOKEN,
+        {
+            expiresIn: "10d",
+        },
+    );
+    return { accessToken, refreshToken };
+};
+
+const saveRefreshToken = async (userId, refreshToken) => {
+    await redis.set(
+        `refresh_token:${userId}`,
+        refreshToken,
+        "EX",
+        7 * 24 * 60 * 60,
+    );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, //protect from XSS hacking
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", //prevents CSRF attacks
+        maxAge: 10 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, //protect from XSS hacking
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", //prevents CSRF attacks
+        maxAge: 10 * 24 * 60 * 60 * 1000,
+    });
+};
 
 const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -9,7 +50,22 @@ const signup = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
         const newUser = await User.create({ name, email, password });
-        res.status(201).json({ newUser, message: "User created successfully" });
+
+        //AUTHENTICATION
+        const { accessToken, refreshToken } = getMyTokens(newUser._id);
+        await saveRefreshToken(newUser._id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({
+            newUser: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role,
+            },
+            message: "User created successfully",
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
---
+++ api/lib/redis.js
@@ -0,0 +1,8 @@
+const Redis = require("ioredis");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const redis = new Redis(process.env.REDIS_URL);
+
+module.exports = redis;
