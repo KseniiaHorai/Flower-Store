dc1e36c9cb3a78b0e5da96d87f852b62a6d50dc9
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2026-05-04 16:17:41 +0300

    add product creation with Cloudinary upload and admin protection

--- api/endpoints/product.route.js
+++ api/endpoints/product.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
     getAllProducts,
     getFeaturedProducts,
+    createProduct,
 } = require("../handlers/product.controller.js");
 const { protectRoute } = require("../middleware/auth.middleware.js");
 const { adminRoute } = require("../middleware/auth.middleware.js");
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 module.exports = router;
--- api/handlers/product.controller.js
+++ api/handlers/product.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../models/Product.js");
+const { redis } = require("../lib/redis.js");
+const claudinary = require("../lib/cloudinary.js");
 
 const getAllProducts = async (req, res) => {
     try {
@@ -33,4 +35,31 @@ const getFeaturedProducts = async (req, res) => {
     }
 };
 
-module.exports = { getAllProducts, getFeaturedProducts };
+const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+
+        let cloudinatyResponse = null;
+
+        if (image) {
+            cloudinatyResponse = await cloudinary.uploader.upload(image, {
+                folder: "myproducts",
+            });
+        }
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinatyResponse?.secure_url
+                ? cloudinatyResponse.secure_url
+                : "",
+            category,
+        });
+        res.status(201).json(product);
+    } catch (error) {
+        console.log("Error in getFeaturedProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+module.exports = { getAllProducts, getFeaturedProducts, createProduct };
